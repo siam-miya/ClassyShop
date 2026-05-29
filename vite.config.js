af0cd2b4b319f98562fe83b247b01a3119c40865
siam-miya <siammiya2024@gamil.com>
2026-05-30 01:57:51 +0600

    feat: react vite pwa setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "ClassyShop",
+        short_name: "ClassyShop",
+        description: "Premium Gadgets and Apparel Shop",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "main-logo.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "main-logo.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "main-logo.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
